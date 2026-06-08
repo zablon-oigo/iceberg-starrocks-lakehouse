cf1dccf7e7316bb3d99f02612b061f4da1ebd354
zablon <zablonoigo51@gmail.com>
2026-06-08 08:34:30 +0300

    write data to table

--- init.sql
+++ init.sql
@@ -11,3 +11,29 @@ PROPERTIES
   "aws.s3.endpoint"="http://minio:9000",
   "aws.s3.enable_path_style_access"="true"
 );
+
+SHOW CATALOGS\G
+
+SET CATALOG demo;
+
+CREATE DATABASE nyc;
+
+USE nyc;
+
+CREATE TABLE demo.nyc.taxis
+(
+    trip_id            bigint,
+    trip_distance      float,
+    fare_amount double,
+    store_and_fwd_flag string,
+    vendor_id          bigint
+) PARTITION BY (vendor_id);
+
+INSERT INTO demo.nyc.taxis
+VALUES (1000371, 1.8, 15.32, 'N', 1),
+       (1000372, 2.5, 22.15, 'N', 2),
+       (1000373, 0.9, 9.01, 'N', 2),
+       (1000374, 8.4, 42.13, 'Y', 1);
+
+SELECT *
+FROM demo.nyc.taxis;
